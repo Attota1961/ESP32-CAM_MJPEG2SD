851a28264bac134384d85d9bba60f6487059e9a2
Attota1961 <167180530+Attota1961@users.noreply.github.com>
2025-06-22 12:25:14 +0530

    Update ESP32-CAM_MJPEG2SD.ino

--- ESP32-CAM_MJPEG2SD.ino
+++ ESP32-CAM_MJPEG2SD.ino
@@ -1,47 +1,59 @@
 /*
-* Capture ESP32 Cam JPEG images into a AVI file and store on SD
-* AVI files stored on the SD card can also be selected and streamed to a browser as MJPEG.
-*
-* s60sc 2020 - 2024
-*/
+ * Capture ESP32-CAM JPEG images into an AVI file and store on SD
+ * AVI files stored on the SD card can also be selected and streamed to a browser as MJPEG.
+ * 
+ * s60sc 2020 - 2024
+ */
 
-#include "appGlobals.h"
+#include "appGlobals.h"  // Ensure this file exists in your include path
+#include <WiFi.h>        // For Wi-Fi IP printout
 
 void setup() {
   logSetup();
   LOG_INF("Selected board %s", CAM_BOARD);
-  // prep storage
+
+  // Initialize SD card and load config
   if (startStorage()) {
-    // Load saved user configuration
     if (loadConfig()) {
 #ifndef AUXILIARY
-      // initialise camera
+      // Initialize camera if PSRAM is available
       if (psramFound()) {
-        if (ESP.getPsramSize() > 1 * ONEMEG) prepCam();
-        else snprintf(startupFailure, SF_LEN, STARTUP_FAIL "Insufficient PSRAM for app: %s", fmtSize(ESP.getPsramSize()));
-      } else snprintf(startupFailure, SF_LEN, STARTUP_FAIL "Need PSRAM to be enabled");
+        if (ESP.getPsramSize() > 1 * ONEMEG) {
+          prepCam();
+        } else {
+          snprintf(startupFailure, SF_LEN, STARTUP_FAIL "Insufficient PSRAM for app: %s", fmtSize(ESP.getPsramSize()));
+        }
+      } else {
+        snprintf(startupFailure, SF_LEN, STARTUP_FAIL "Need PSRAM to be enabled");
+      }
 #else
       LOG_INF("AUXILIARY mode without camera");
 #endif
     }
   }
-  
+
 #ifdef DEV_ONLY
   devSetup();
 #endif
-
-  // connect wifi or start config AP if router details not available
+  // Connect to Wi-Fi or start AP
   startWifi();
 
+  // Start web server
   startWebServer();
-  if (strlen(startupFailure)) LOG_WRN("%s", startupFailure);
-  else {
-    // start rest of services
+
+  // Print IP address for easy access
+  Serial.print("ESP32 IP Address: http://");
+  Serial.println(WiFi.localIP());
+
+  if (strlen(startupFailure)) {
+    LOG_WRN("%s", startupFailure);
+  } else {
 #ifndef AUXILIARY
-    startSustainTasks(); 
+    startSustainTasks();
 #endif
+
 #if INCLUDE_SMTP
-    prepSMTP(); 
+    prepSMTP();
 #endif
 #if INCLUDE_FTP_HFS
     prepUpload();
@@ -51,18 +63,18 @@ void setup() {
 #endif
 #if INCLUDE_PERIPH
     prepPeripherals();
-  #if INCLUDE_MCPWM 
+  #if INCLUDE_MCPWM
     prepMotors();
   #endif
 #endif
 #if INCLUDE_AUDIO
-    prepAudio(); 
+    prepAudio();
 #endif
 #if INCLUDE_TGRAM
     prepTelegram();
 #endif
 #if INCLUDE_I2C
-  prepI2C();
+    prepI2C();
   #if INCLUDE_TELEM
     prepTelemetry();
   #endif
@@ -71,21 +83,20 @@ void setup() {
     startHeartbeat();
 #endif
 #ifndef AUXILIARY
- #if INCLUDE_RTSP
+  #if INCLUDE_RTSP
     prepRTSP();
- #endif
- if (!prepRecording()) {
-   snprintf(startupFailure, SF_LEN, STARTUP_FAIL "Insufficient memory, remove optional features");
-   LOG_WRN("%s", startupFailure);
- }
+  #endif
+    if (!prepRecording()) {
+      snprintf(startupFailure, SF_LEN, STARTUP_FAIL "Insufficient memory, remove optional features");
+      LOG_WRN("%s", startupFailure);
+    }
 #endif
     checkMemory();
-  } 
+  }
 }
 
 void loop() {
-  // confirm not blocked in setup
   LOG_INF("=============== Total tasks: %u ===============\n", uxTaskGetNumberOfTasks() - 1);
   delay(1000);
-  vTaskDelete(NULL); // free 8k ram
+  vTaskDelete(NULL);  // Free 8k RAM
 }
